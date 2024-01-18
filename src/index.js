3a0946de39db35f6eb4320f63484cee410305974
Zahari Lazarov <harilazaro@gmail.com>
2024-01-18 11:51:06 +0200

    Start handlebars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const port = 5001;
 const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './views'));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        layout: false,
+    });
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
