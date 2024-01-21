d4e19a5e67f60819acb08e9d9cd81382e99b84f8
Zahari Lazarov <harilazaro@gmail.com>
2024-01-21 16:58:05 +0200

    Movies details working

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -11,4 +11,11 @@ router.post('/movies/create', (req, res) => {
     res.redirect('/');
 });
 
+router.get('/movies/:movieId', (req, res) => {
+    const movieInfo = movieService.getMovie(req.params.movieId);
+    const ratingArray = new Array(Number(movieInfo.rating)).fill(true);
+    // &#x2605;
+    res.render('details', { movieInfo, ratingArray });
+});
+
 module.exports = router;
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,7 +5,7 @@ const movies = [
         genre: 'Action',
         director: 'Christopher McQuarrie',
         year: '2023',
-        imageUrl: 'img/mission-impossible-2023.jpeg',
+        imageUrl: '/img/mission-impossible-2023.jpeg',
         rating: 4,
         description: `Ethan Hunt (Tom Cruise) and his IMF team embark on their most dangerous mission yet: To track down a terrifying new weapon that threatens all of humanity before it falls into the wrong hands. With control of the future and the fate of the world at stake, and dark forces from Ethan's past closing in, a deadly race around the globe begins. Confronted by a mysterious, all-powerful enemy, Ethan is forced to consider that nothing can matter more than his mission -- not even the lives of those he cares about most.`
     },
@@ -15,7 +15,7 @@ const movies = [
         genre: 'Action',
         director: 'Colin Trevorrow',
         year: '2022',
-        imageUrl: 'img/jurassic-world-dominion.jpeg',
+        imageUrl: '/img/jurassic-world-dominion.jpeg',
         rating: 3,
         description: `Dominion takes place four years after Isla Nublar has been destroyed. Dinosaurs now live--and hunt--alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.`
     },
@@ -25,7 +25,7 @@ const movies = [
         genre: 'Animation',
         director: 'Mark Dindal',
         year: '2024',
-        imageUrl: 'img/the-garfield-movie.jpeg',
+        imageUrl: '/img/the-garfield-movie.jpeg',
         rating: 0,
         description: 'Garfield has an unexpected reunion with his long-lost father, a scruffy street cat who draws him into a high-stakes heist.'
     },
@@ -35,7 +35,12 @@ exports.getAllMovies = () => {
     return movies.slice();
 };
 
+exports.getMovie = (id) => {
+    return movies.find(movie => movie._id == id);
+};
+
 exports.addMovie = (movieInfo) => {
-    movieInfo._id = movies.length == 0 ? 1 : movies[movies.length - 1]._id + 1; 
+    movieInfo.rating = Number(movieInfo.rating);
+    movieInfo._id = movies.length == 0 ? 1 : movies[movies.length - 1]._id + 1;
     movies.push(movieInfo);
-}
+};
