33a3d5aa68227c6c427ddc57eac64ba1266ccde6
Zahari Lazarov <harilazaro@gmail.com>
2024-02-04 21:20:18 +0200

    Delete import

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
 const movieController = require('./controllers/movieController');
 const castController = require('./controllers/castController');
-const { auth } = require('./middlewares/authMiddleware');
 
 router.use(homeController);
 router.use('/auth', authController);
