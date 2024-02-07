a24cc6d71c44a4c88b9e59810c929784290a8621
Zahari Lazarov <harilazaro@gmail.com>
2024-02-07 14:24:35 +0200

    Add cast error handling

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -2,14 +2,22 @@ const router = require('express').Router();
 
 const castService = require('../services/castService');
 const { isAuth } = require('../middlewares/authMiddleware');
+const { getErrorMessage } = require('../utils/errorUtils');
 
 router.get('/create', isAuth, (req, res) => {
     res.render('cast/create');
 });
 
 router.post('/create', isAuth, async (req, res) => {
-    await castService.addCast(req.body);
-    res.redirect('/');
+    const castInfo = req.body;
+
+    try {
+        await castService.addCast(castInfo);
+        res.redirect('/');
+    } catch (err) {
+        const message = getErrorMessage(err);
+        res.render('cast/create', { ...castInfo, error: message });
+    }
 });
 
 module.exports = router;
--- src/models/Cast.js
+++ src/models/Cast.js
@@ -10,27 +10,27 @@ const castSchema = new mongoose.Schema({
     },
     age: {
         type: Number,
-        required: true,
+        required: [true, 'Age is required'],
         min: [1, 'The minimum age is 1'],
         max: [120, 'The maximum age is 120']
     },
     born: {
         type: String,
-        required: true,
+        required: [true, 'Born is required'],
         match: [/[a-zA-Z0-9\s]+/, 'The born should consist only of English letters, digits and whitespace characters'],
         minLength: [10, 'The born should be at least 10 characters long'],
         trim: true
     },
     nameInMovie: {
         type: String,
-        required: true,
+        required: [true, 'Name in Movie is required'],
         match: [/[a-zA-Z0-9\s]+/, 'The name in movie should consist only of English letters, digits and whitespace characters'],
         minLength: [5, 'The name in movie should be at least 5 characters long'],
         trim: true
     },
     imageUrl: {
         type: String,
-        required: true,
+        required: [true, 'Cast Image is required'],
         match: [
             /https?:\/\//i,
             `The cast image URL should start with "http://" or "https://"`
