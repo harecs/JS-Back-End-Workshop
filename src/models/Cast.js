3e847bdef873dfd3ac3cdf5a4749c19c0978f5d6
Zahari Lazarov <harilazaro@gmail.com>
2024-02-07 14:07:55 +0200

    Add Cast model validation

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -3,28 +3,37 @@ const mongoose = require('mongoose');
 const castSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Name is required'],
+        match: [/[a-zA-Z0-9\s]+/, 'The name should consist only of English letters, digits and whitespace characters'],
+        minLength: [5, 'The name should be at least 5 characters long'],
+        trim: true
     },
     age: {
         type: Number,
         required: true,
-        min: 1,
-        max: 125
+        min: [1, 'The minimum age is 1'],
+        max: [120, 'The maximum age is 120']
     },
     born: {
         type: String,
-        required: true
+        required: true,
+        match: [/[a-zA-Z0-9\s]+/, 'The born should consist only of English letters, digits and whitespace characters'],
+        minLength: [10, 'The born should be at least 10 characters long'],
+        trim: true
     },
     nameInMovie: {
         type: String,
-        required: true
+        required: true,
+        match: [/[a-zA-Z0-9\s]+/, 'The name in movie should consist only of English letters, digits and whitespace characters'],
+        minLength: [5, 'The name in movie should be at least 5 characters long'],
+        trim: true
     },
     imageUrl: {
         type: String,
         required: true,
         match: [
             /https?:\/\//i,
-            `The image URL does't start with "http://" or "https://"`
+            `The cast image URL should start with "http://" or "https://"`
         ]
     },
     movie: {
